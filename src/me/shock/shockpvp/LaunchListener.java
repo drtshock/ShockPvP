9399a1a16a57aa14682a7cf39b68170387f0b478
Trent Hensler <thensler@wisc.edu>
2012-12-04 16:57:33 -0600

    Adding projectile hit event.

--- src/me/shock/shockpvp/LaunchListener.java
+++ src/me/shock/shockpvp/LaunchListener.java
@@ -1,13 +1,18 @@
 package me.shock.shockpvp;
 
+import java.util.List;
+
 import org.bukkit.Effect;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class LaunchListener implements Listener
 {
@@ -19,8 +24,10 @@ public class LaunchListener implements Listener
 		plugin = instance;
 	}
 	
+	double flashRadius;
+	
 	@EventHandler
-	public void onBowShoot(ProjectileLaunchEvent event)
+	public void onLaunch(ProjectileLaunchEvent event)
 	{
 		LivingEntity shooter = event.getEntity().getShooter();
 		EntityType entity = event.getEntityType();
@@ -37,4 +44,47 @@ public class LaunchListener implements Listener
 		}
 	}
 	
+	/*
+	 * Grenades
+	 * - Flash
+	 * - Concussion
+	 * - Frag
+	 * - Smoke
+	 * - Decoy
+	 */
+	
+	@EventHandler
+	public void onHit(ProjectileHitEvent event)
+	{
+		// Get the shooter.
+		LivingEntity shooter = event.getEntity().getShooter();
+		
+		// Get the entity shot.
+		EntityType entity = event.getEntityType();
+		
+		// Check if it's a player.
+		if(shooter instanceof Player)
+		{
+			// Get the Player
+			Player player = (Player) shooter;
+			
+			// Snowball
+			if(entity.equals(EntityType.SNOWBALL))
+			{
+				// Flash grenade.
+				if(player.hasPermission("shockpvp.grenade.flash"))
+				{
+					Location loc = event.getEntity().getLocation();
+					List<Entity> entities = event.getEntity().getNearbyEntities(flashRadius, flashRadius, flashRadius);
+					
+					// Blind the nearby entities.
+					for(Entity entity : entities)
+					{
+						
+					}
+				}
+			}
+		}
+		
+	}
 }
